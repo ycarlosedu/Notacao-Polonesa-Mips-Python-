363ab81a9a87eae3fec97ec0296a31da03927057
Carlos Eduardo <silvacarlosoliveira@outlook.com>
2021-10-14 22:49:18 -0300

    funciona um pouco

--- compiler.py
+++ compiler.py
@@ -1,11 +1,11 @@
-operacao = input("Digite idade: ").split()
+operacao = input("Digite o calculo que deseja realizar (em notação polonesa, separado por espaços): ").split()
 
 arquivo = open('./teste.txt', 'w')
 arquivo.truncate()
 
 def EscreveSoma(somador1, somador2):
-  string = "li $t0 " + somador1 + "\n"
-  string2 = "li $t1 " + somador2 + "\n"
+  string = "li $t0 " + str(somador1) + "\n"
+  string2 = "li $t1 " + str(somador2) + "\n"
   string3 = "add $t0 $t0 $t1 \n"
   arquivo.writelines(string) 
   arquivo.writelines(string2) 
@@ -14,8 +14,8 @@ def EscreveSoma(somador1, somador2):
   return str(resultado)
 
 def EscreveSub(somador1, somador2):
-  string = "li $t0 " + somador1 + "\n"
-  string2 = "li $t1 " + somador2 + "\n"
+  string = "li $t0 " + str(somador1) + "\n"
+  string2 = "li $t1 " + str(somador2) + "\n"
   string3 = "sub $t0 $t0 $t1 \n"
   arquivo.writelines(string) 
   arquivo.writelines(string2) 
@@ -24,8 +24,8 @@ def EscreveSub(somador1, somador2):
   return str(resultado)
 
 def EscreveMulti(somador1, somador2):
-  string = "li $t0 " + somador1 + "\n"
-  string2 = "li $t1 " + somador2 + "\n"
+  string = "li $t0 " + str(somador1) + "\n"
+  string2 = "li $t1 " + str(somador2) + "\n"
   string3 = "multi $t0 $t0 $t1 \n"
   arquivo.writelines(string) 
   arquivo.writelines(string2) 
@@ -34,8 +34,8 @@ def EscreveMulti(somador1, somador2):
   return str(resultado)
   
 def EscreveDiv(somador1, somador2):
-  string = "li $t0 " + somador1 + "\n"
-  string2 = "li $t1 " + somador2 + "\n"
+  string = "li $t0 " + str(somador1) + "\n"
+  string2 = "li $t1 " + str(somador2) + "\n"
   string3 = "div $t0 $t0 $t1 \n"
   arquivo.writelines(string) 
   arquivo.writelines(string2) 
@@ -44,8 +44,8 @@ def EscreveDiv(somador1, somador2):
   return str(resultado)
 
 def EscreveFator(somador1, somador2):
-  string = "li $t0 " + somador1 + "\n"
-  string2 = "li $t1 " + somador2 + "\n"
+  string = "li $t0 " + str(somador1) + "\n"
+  string2 = "li $t1 " + str(somador2) + "\n"
   string3 = "sub $t0 $t0 $t1 \n"
   arquivo.writelines(string) 
   arquivo.writelines(string2) 
@@ -54,8 +54,8 @@ def EscreveFator(somador1, somador2):
   return str(resultado)
 
 def EscreveRaiz(somador1, somador2):
-  string = "li $t0 " + somador1 + "\n"
-  string2 = "li $t1 " + somador2 + "\n"
+  string = "li $t0 " + str(somador1) + "\n"
+  string2 = "li $t1 " + str(somador2) + "\n"
   string3 = "sub $t0 $t0 $t1 \n"
   arquivo.writelines(string) 
   arquivo.writelines(string2) 
@@ -73,48 +73,61 @@ def EscrevePotencia(somador1, somador2):
   resultado = int(somador1) - int(somador2)
   return str(resultado)
 
-def Finaliza():
+def Finaliza(resultado):
   string = "move $a0, $t0\n"
   string2 = "li $v0, 1\n"
   string3 = "syscall\n"
   arquivo.writelines(string) 
   arquivo.writelines(string2) 
   arquivo.writelines(string3) 
+  print("resultado:" + resultado)
 
 def VerificaDigito(digito, resultado):
   if digito.isdigit():
     numeros.append(digito)
+    global contador
+    contador = contador + 1
+    # print(numeros)
+    # print(contador)
   else:
     if digito == "+":
-      if resultado == 0:
+      if contador >= 2:
+        contador = 0
         resultado = EscreveSoma(numeros[len(numeros)-2], numeros[len(numeros)-1])
         numeros.remove(numeros[len(numeros)-2])
         numeros.remove(numeros[len(numeros)-1])
       else:
+        contador = 0
         resultado = EscreveSoma(resultado, numeros[len(numeros)-1])
         numeros.remove(numeros[len(numeros)-1])
     elif digito == "-":
-      if resultado == 0:
-        EscreveSub(numeros[len(numeros)-2], numeros[len(numeros)-1])
+      if contador >= 2:
+        contador = 0
+        resultado = EscreveSub(numeros[len(numeros)-2], numeros[len(numeros)-1])
         numeros.remove(numeros[len(numeros)-2])
         numeros.remove(numeros[len(numeros)-1])
       else:
+        contador = 0
         resultado = EscreveSub(resultado, numeros[len(numeros)-1])
         numeros.remove(numeros[len(numeros)-1])
     elif digito == "/":
-      if resultado == 0:
-        EscreveDiv(numeros[len(numeros)-2], numeros[len(numeros)-1])
+      if contador >= 2:
+        contador = 0
+        resultado = EscreveDiv(numeros[len(numeros)-2], numeros[len(numeros)-1])
         numeros.remove(numeros[len(numeros)-2])
         numeros.remove(numeros[len(numeros)-1])
       else:
+        contador = 0
         resultado = EscreveDiv(resultado, numeros[len(numeros)-1])
         numeros.remove(numeros[len(numeros)-1])
     elif digito == "*":
-      if resultado == 0:
-        EscreveMulti(numeros[len(numeros)-2], numeros[len(numeros)-1])
+      if contador >= 2:
+        contador = 0
+        resultado = EscreveMulti(numeros[len(numeros)-2], numeros[len(numeros)-1])
         numeros.remove(numeros[len(numeros)-2])
         numeros.remove(numeros[len(numeros)-1])
       else:
+        contador = 0
         resultado = EscreveMulti(resultado, numeros[len(numeros)-1])
         numeros.remove(numeros[len(numeros)-1])
     elif digito == "f":
@@ -127,8 +140,10 @@ def VerificaDigito(digito, resultado):
   
 numeros = []
 resultado = 0
+contador = 0
 for i in range(len(operacao)):
   resultado = VerificaDigito(operacao[i], resultado)
-Finaliza()
+  
+Finaliza(resultado)
 
 arquivo.close() 
